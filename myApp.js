a33ada84c3284b619d9617906643e7b3867de67d
Villegas, Edgar <Edgar.Villegas@mckesson.com>
2023-11-11 08:00:28 -0800

    Add view

--- myApp.js
+++ myApp.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 
 app.get("/", function(req, res) {
-    res.send("Hello Express");
+    const view = __dirname + "/views/index.html";
+    res.sendFile(view);
 });
 
 module.exports = app;
