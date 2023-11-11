6c8ae6c1be24ead4f932aae2a1879632b62b927c
Villegas, Edgar <Edgar.Villegas@mckesson.com>
2023-11-11 08:09:04 -0800

    Add public path

--- myApp.js
+++ myApp.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+app.use("/public", express.static(__dirname + "/public"))
+
 app.get("/", function(req, res) {
     const view = __dirname + "/views/index.html";
     res.sendFile(view);
