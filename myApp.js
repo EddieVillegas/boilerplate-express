5fd7bdc7638567b3d84c685867c0e6cfe3de7621
Villegas, Edgar <Edgar.Villegas@mckesson.com>
2023-12-06 22:27:08 -0600

    Get Query Parameter Input from the Client

--- myApp.js
+++ myApp.js
@@ -7,6 +7,11 @@ const logger = function(req, res, next) {
     next()
 }
 
+app.route('/name').get((req, res) => {
+    const { firstname, lastname } = req?.query;
+    res.json({ name: `${firstname} ${lastname}`})
+})
+
 app.use('/:word/echo', (req, res) => {
     const {params: { word: echo }} = req;
     res.json({ echo });
